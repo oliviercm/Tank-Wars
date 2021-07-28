55bc547a606886002e45eadf62c7d07be30af424
Olivier Chan <olivierchanmoy@gmail.com>
2021-07-27 20:55:25 -0700

    Add example code

---
+++ src/tankrotationexample/GameConstants.java
@@ -0,0 +1,12 @@
+package tankrotationexample;
+
+public class GameConstants {
+    public static final int GAME_SCREEN_WIDTH = 1024;
+    public static final int GAME_SCREEN_HEIGHT = 768;
+
+    public static final int START_MENU_SCREEN_WIDTH = 500;
+    public static final int START_MENU_SCREEN_HEIGHT = 550;
+
+    public static final int END_MENU_SCREEN_WIDTH = 500;
+    public static final int END_MENU_SCREEN_HEIGHT = 500;
+}
---
+++ src/tankrotationexample/game/TRE.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tankrotationexample.game;
+
+
+import tankrotationexample.GameConstants;
+import tankrotationexample.Launcher;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
+
+
+import static javax.imageio.ImageIO.read;
+
+/**
+ *
+ * @author anthony-pc
+ */
+public class TRE extends JPanel implements Runnable {
+
+    private BufferedImage world;
+    private Tank t1;
+    private Launcher lf;
+    private long tick = 0;
+
+    public TRE(Launcher lf){
+        this.lf = lf;
+    }
+
+    @Override
+    public void run(){
+       try {
+           this.resetGame();
+           while (true) {
+                this.tick++;
+                this.t1.update(); // update tank
+                this.repaint();   // redraw game
+                Thread.sleep(1000 / 144); //sleep for a few milliseconds
+                /*
+                 * simulate an end game event
+                 * we will do this with by ending the game when drawn 2000 frames have been drawn
+                 */
+                if(this.tick > 2000){
+                    this.lf.setFrame("end");
+                    return;
+                }
+            }
+       } catch (InterruptedException ignored) {
+           System.out.println(ignored);
+       }
+    }
+
+    /**
+     * Reset game to its initial state.
+     */
+    public void resetGame(){
+        this.tick = 0;
+        this.t1.setX(300);
+        this.t1.setY(300);
+    }
+
+
+    /**
+     * Load all resources for Tank Wars Game. Set all Game Objects to their
+     * initial state as well.
+     */
+    public void gameInitialize() {
+        this.world = new BufferedImage(GameConstants.GAME_SCREEN_WIDTH,
+                                       GameConstants.GAME_SCREEN_HEIGHT,
+                                       BufferedImage.TYPE_INT_RGB);
+
+        BufferedImage t1img = null;
+        try {
+            /*
+             * note class loaders read files from the out folder (build folder in Netbeans) and not the
+             * current working directory.
+             */
+            t1img = read(Objects.requireNonNull(TRE.class.getClassLoader().getResource("tank1.png")));
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+
+        t1 = new Tank(300, 300, 0, 0, 0, t1img);
+        TankControl tc1 = new TankControl(t1, KeyEvent.VK_UP, KeyEvent.VK_DOWN, KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT, KeyEvent.VK_ENTER);
+        this.setBackground(Color.BLACK);
+        this.lf.getJf().addKeyListener(tc1);
+    }
+
+
+    @Override
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        Graphics2D buffer = world.createGraphics();
+        buffer.setColor(Color.BLACK);
+        buffer.fillRect(0,0,GameConstants.GAME_SCREEN_WIDTH,GameConstants.GAME_SCREEN_HEIGHT);
+        this.t1.drawImage(buffer);
+        g2.drawImage(world,0,0,null);
+    }
+
+}
